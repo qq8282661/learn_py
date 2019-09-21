adb74f84bd2ea3c3debf02cac27777858f878dc2
邵华 <644827059@qq.com>
2019-09-21 14:30:39 +0800

    ++++

--- multiple.py
+++ multiple.py
@@ -48,8 +48,11 @@ class Ostrich(Bird):
 # Python自带的很多库也使用了MixIn。举个例子，Python自带了TCPServer和UDPServer这两类网络服务，
 # 而要同时服务多个用户就必须使用多进程或多线程模型，
 # 这两种模型由ForkingMixIn和ThreadingMixIn提供。通过组合，我们就可以创造出合适的服务来。
+
+
 class MyTCPServer(TCPServer, ForkingMixIn):
     pass
+
 
 class MyUDPServer(UDPServer, ThreadingMixIn):
     pass
